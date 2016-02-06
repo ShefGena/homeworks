1ac06a5aa0f867f3928687f66264697963ccbc81
Shef Gena <shef_gena@ukr.net>
2016-02-06 15:01:54 +0200

    Commit 6/02/15

---
+++ DEV19.php
@@ -0,0 +1,12 @@
+<?php
+$country = array(
+								'Uraine' => Kiev,	
+								'Moldova' => Chisinau,	
+								'USA' => Washington,	
+								'Spain' => Madrid,	
+								'Germany' => Berlin
+								);
+echo '<pre>';
+print_r ($country);
+echo '</pre>';
+?>
