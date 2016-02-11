6b0113c43020c9eadb316ca234e1ae775d6ce5e1
Shef Gena <shef_gena@ukr.net>
2016-02-11 18:49:11 +0200

    Commit

--- arrays_loops_tasks/3.php
+++ arrays_loops_tasks/3.php
@@ -4,7 +4,20 @@ header('Content-Type: text/html; charset=utf-8');
 $a = array('26', '17', '136', '12', '79', '15');
 foreach($a as $b){
 	$c[] = sqrt($b);
-	//echo array_sum($c) . '<br>';
 }
 	echo array_sum($c) . '<br>';
+?>
+
+
+<?php
+error_reporting(E_ALL);
+//header('Content-Type: text/html; charset=utf-8');
+$result = 0;
+$arr = array('26', '17', '136', '12', '79', '15');
+foreach ($arr as $v) {
+$sq = sqrt($v);
+echo (is_float($sq));
+$result += $sq;
+}
+echo "Сумма элементов массива равна $result";
 ?>
